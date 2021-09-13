f01ed84ff9dfeb25c2eeca4a47b80b0eff187758
urao123 <kuttam185@gmail.com>
2021-09-13 01:37:50 -0500

    Add files via upload

---
+++ Development Project/ProductModel.cs
@@ -0,0 +1,44 @@
+﻿namespace Interview.Web.models
+{
+    /// <summary>
+    /// class for product model
+    /// </summary>
+    public class ProductModel : Interview.Web.Models.BaseModel
+    {
+        /// <summary>
+        /// product id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// product name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// product description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// product image Uri's
+        /// </summary>
+        public string ImageUris { get; set; }
+
+        /// <summary>
+        /// product color
+        /// </summary>
+
+        public string Color { get; set; }
+
+        /// <summary>
+        /// valid sku's
+        /// </summary>
+        public string ValidSkus { get; set; }
+
+        /// <summary>
+        /// to know product is active or discontinued
+        /// </summary>
+        public Boolean Active { get; set; }
+    }
+}
