de95a30f7e14b4305010c331339b5f47f3e2c93d
urao123 <kuttam185@gmail.com>
2021-09-13 01:39:56 -0500

    Add files via upload
    Models

---
+++ BaseModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Interview.Web.Models
+{
+    /// <summary>
+    /// class for base model
+    /// </summary>
+    public class BaseModel
+    {
+        /// <summary>
+        /// created Date
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Updated Date
+        /// </summary>
+        public DateTime? UpdatedDate { get; set; }
+
+        /// <summary>
+        /// Updated By
+        /// </summary>
+        public string UpdatedBy { get; set; }
+    }
+}
