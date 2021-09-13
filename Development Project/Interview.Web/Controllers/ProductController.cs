42d01a51570922f642715540d178149e317530e2
urao123 <kuttam185@gmail.com>
2021-09-13 01:34:29 -0500

    Update ProductController.cs

--- Development Project/Interview.Web/Controllers/ProductController.cs
+++ Development Project/Interview.Web/Controllers/ProductController.cs
@@ -1,19 +1,87 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Interview.Web.models;
+using Interview.Web.Services;
 
 namespace Interview.Web.Controllers
 {
     [Route("api/v1/products")]
     public class ProductController : Controller
     {
-        // NOTE: Sample Action
+        /// <summary>
+        /// Contains the service
+        /// </summary>
+        private readonly IProductService _service;
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="service">The service.</param>
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+        /// <summary>
+        // to get product list
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/v1/products/getproducts")]
         [HttpGet]
         public Task<IActionResult> GetAllProducts()
         {
-            return Task.FromResult((IActionResult)Ok(new object[] { }));
+            var products = this._service.GetAllProducts();
+            return Task.FromResult((IActionResult)Ok(products));
+        }
+
+        /// <summary>
+        /// To get product by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route("api/v1/products/getproductsbyid")]
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            var products = this._service.GetProductById(id);
+            return Task.FromResult((IActionResult)Ok(products));
+        }
+
+
+        /// <summary>
+        /// Add products
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [Route("api/v1/products/addproduct")]
+        [HttpPost]
+        public Task<IActionResult> AddProduct([FromBody] ProductModel data)
+        {
+         this._service.AddProduct(data);
+            return Task.FromResult((IActionResult)Ok(data));
+        }
+
+        /// <summary>
+        /// Add products
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [Route("api/v1/products/updateproduct")]
+        [HttpPost]
+        public Task<IActionResult> UpdateProduct([FromBody] ProductModel data)
+        {
+            this._service.UpdateProduct(data);
+            return Task.FromResult((IActionResult)Ok(data));
+        }
+
+        /// <summary>
+        /// Add products
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [Route("api/v1/products/deleteproduct")]
+        [HttpDelete]
+        public Task<IActionResult> DeleteProduct([FromBody] ProductModel data)
+        {
+            this._service.DeleteProduct(data);
+            return Task.FromResult((IActionResult)Ok(data));
         }
     }
 }
