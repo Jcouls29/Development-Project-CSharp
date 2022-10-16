54f146587839b55dbbf77d043ad706994f7eb50a
Matt Kinsey <mattkinsey96@gmail.com>
2022-10-16 09:59:04 -0400

    Added basic Product model and repository abstraction

---
+++ Development Project/Sparcpoint.Core/Abstract/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sparcpoint.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparcpoint.Abstract
+{
+    public interface IProductRepository
+    {
+        Task<bool> AddProductAsync(Product product);
+    }
+}
---
+++ Development Project/Sparcpoint.Core/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sparcpoint.Models
+{
+    public class Product
+    {
+        public int InstanceId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ProductImageUris { get; set; }
+        public string ValidSkus { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+    }
+}
