030936c5b73f0edbbd52d3fa82d46d7573363bab
Matt Kinsey <mattkinsey96@gmail.com>
2022-10-16 10:11:03 -0400

    Added basic Category model and repository abstraction

---
+++ Development Project/Sparcpoint.Core/Abstract/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sparcpoint.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparcpoint.Abstract
+{
+    public interface ICategoryRepository
+    {
+        Task<bool> AddCategoryAsync(Category category);
+    }
+}
