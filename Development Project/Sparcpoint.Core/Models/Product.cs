7267d2f9cf140771c8c3e5b94a846b699e634199
karthik dhanekula <karthik06093@gmail.com>
2022-11-06 23:18:39 -0500

    Added model for Product and Product attributes

---
+++ Development Project/Sparcpoint.Core/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sparcpoint.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ProductImageUris { get; set; }
+        public string ValidSkus { get; set; }
+        public List<ProductAttributes> ProductAttributes { get; set; }
+    }
+
+    public class ProductAttributes
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
