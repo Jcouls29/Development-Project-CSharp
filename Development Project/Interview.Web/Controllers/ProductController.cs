299e61e616ed2d5388b6e99b80e203465fafbb33
Wade <wadefade@gmail.com>
2023-02-20 21:00:48 -0700

    Added stub for AllProducts

--- Development Project/Interview.Web/Controllers/ProductController.cs
+++ Development Project/Interview.Web/Controllers/ProductController.cs
@@ -24,43 +24,10 @@ namespace Interview.Web.Controllers
         [Route("api/v1/products/all")]
         public async Task<IActionResult> GetAllProducts()
         {
-            var products = new List<Product>()
-            {
-                new Product(){
-                    ProductId = Guid.NewGuid(),
-                    Name = "Existing Product 1",
-                    Description = "I'm a description",
-                    Categories = new Dictionary<string, int>() { { "Blue", 1 }, { "Sale", 1 } },
-                    MetaData = new Dictionary<string, object>() { { "Sku", 19932 }, { "Color", "Blue" } },
-                    IsDeleted = false
-                },
-                new Product(){
-                    ProductId = Guid.NewGuid(),
-                    Name = "Existing Product 2",
-                    Description = "I'm a description for #2",
-                    Categories = new Dictionary<string, int>() { { "Red", 2 }, { "Sale", 0 } },
-                    MetaData = new Dictionary<string, object>() { { "Sku", 4566 }, { "Color", "Red" } },
-                    IsDeleted = false
-                },
-                new Product(){
-                    ProductId = Guid.NewGuid(),
-                    Name = "Existing Product 3",
-                    Description = "I'm a description but longer",
-                    Categories = new Dictionary<string, int>() { { "Green", 3 }, { "Sale", 0 } },
-                    MetaData = new Dictionary<string, object>() { { "Sku", 4654 }, { "Color", "Green" } },
-                    IsDeleted = false
-                },
-                new Product(){
-                    ProductId = Guid.NewGuid(),
-                    Name = "Existing Product 4",
-                    Description = "I'm a description but I was deleted",
-                    Categories = new Dictionary<string, int>() { { "Purple", 5 }, { "Sale", 2 } },
-                    MetaData = new Dictionary<string, object>() { { "Sku", 463454 }, { "Color", "Purple" } },
-                    IsDeleted = true
-                }
-            };
+            //EVAL: Assuming we only want products not deleted (IsDeleted = False)
+            var products = await _productService.AllProduct();
 
-            return await Task.FromResult((IActionResult)Ok(products.Where(x => !x.IsDeleted)));
+            return await Task.FromResult((IActionResult)Ok(products));
         }
 
         [HttpPost]
--- Development Project/Interview.Web/Services/ProductService.cs
+++ Development Project/Interview.Web/Services/ProductService.cs
@@ -3,6 +3,7 @@ using Sparcpoint.Entities;
 using Sparcpoint.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace Interview.Web.Services
     public interface IProductService
     {
         Task<Product> AddProduct(ProductModel productModel);
+        Task<List<Product>> AllProduct();
         Task<Product> DeleteProduct(Guid productId);
         Task<bool> ProductExists(Guid productId);
         Task<Product> UpdateProduct(ProductModel productModel);
@@ -23,6 +25,55 @@ namespace Interview.Web.Services
         {
             _mapper = mapper;
         }
+
+        public async Task<List<Product>> AllProduct()
+        {
+            var products = await Task.Run(() =>
+            {
+
+                //var products = _db.Product.Where(x => !x.IsDeleted).ToList();
+                var products = new List<Product>()
+                {
+                    new Product(){
+                        ProductId = Guid.NewGuid(),
+                        Name = "Existing Product 1",
+                        Description = "I'm a description",
+                        Categories = new Dictionary<string, int>() { { "Blue", 1 }, { "Sale", 1 } },
+                        MetaData = new Dictionary<string, object>() { { "Sku", 19932 }, { "Color", "Blue" } },
+                        IsDeleted = false
+                    },
+                    new Product(){
+                        ProductId = Guid.NewGuid(),
+                        Name = "Existing Product 2",
+                        Description = "I'm a description for #2",
+                        Categories = new Dictionary<string, int>() { { "Red", 2 }, { "Sale", 0 } },
+                        MetaData = new Dictionary<string, object>() { { "Sku", 4566 }, { "Color", "Red" } },
+                        IsDeleted = false
+                    },
+                    new Product(){
+                        ProductId = Guid.NewGuid(),
+                        Name = "Existing Product 3",
+                        Description = "I'm a description but longer",
+                        Categories = new Dictionary<string, int>() { { "Green", 3 }, { "Sale", 0 } },
+                        MetaData = new Dictionary<string, object>() { { "Sku", 4654 }, { "Color", "Green" } },
+                        IsDeleted = false
+                    },
+                    new Product(){
+                        ProductId = Guid.NewGuid(),
+                        Name = "Existing Product 4",
+                        Description = "I'm a description but I was deleted",
+                        Categories = new Dictionary<string, int>() { { "Purple", 5 }, { "Sale", 2 } },
+                        MetaData = new Dictionary<string, object>() { { "Sku", 463454 }, { "Color", "Purple" } },
+                        IsDeleted = true
+                    }
+            };
+
+                return products.Where(x => !x.IsDeleted).ToList();
+            });
+
+            return products;
+        }
+
         public async Task<Product> AddProduct(ProductModel productModel)
         {
            var addedProduct = await Task.Run(() =>
