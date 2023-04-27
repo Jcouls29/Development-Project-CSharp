27127dffabb2020f9c2d732540bcbd092e8faa70
Taylor Dicks <itayluh@gmail.com>
2023-04-26 21:39:42 -0400

    adding controller operations

--- Development Project/Interview.Web/Controllers/ProductController.cs
+++ Development Project/Interview.Web/Controllers/ProductController.cs
@@ -9,9 +9,38 @@ namespace Interview.Web.Controllers
     [Route("api/v1/products")]
     public class ProductController : Controller
     {
-        // NOTE: Sample Action
         [HttpGet]
         public Task<IActionResult> GetAllProducts()
+        {
+            return Task.FromResult((IActionResult)Ok(new object[] { }));
+        }
+
+        [HttpPost]
+        public Task<IActionResult> AddNewProduct()
+        {
+            return Task.FromResult((IActionResult)Ok(new object[] { }));
+        }
+
+        [HttpPost]
+        public Task<IActionResult> SearchProduct()
+        {
+            return Task.FromResult((IActionResult)Ok(new object[] { }));
+        }
+
+        [HttpPut]
+        public Task<IActionResult> AddProductToInventory()
+        {
+            return Task.FromResult((IActionResult)Ok(new object[] { }));
+        }
+
+        [HttpDelete]
+        public Task<IActionResult> RemoveProductFromInventory()
+        {
+            return Task.FromResult((IActionResult)Ok(new object[] { }));
+        }
+
+        [HttpGet]
+        public Task<IActionResult> RetrieveProductCount()
         {
             return Task.FromResult((IActionResult)Ok(new object[] { }));
         }
