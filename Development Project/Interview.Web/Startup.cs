b6a53b1106af12e222db517891286ffe289b5fa5
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-06-19 00:56:22 -0400

    configure swagger api documentation

--- Development Project/Interview.Web/Startup.cs
+++ Development Project/Interview.Web/Startup.cs
@@ -1,29 +1,90 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Sparcpoint.Core.Persistence.Entity.Sparcpoint.Context;
+using Sparcpoint.Infrastructure.Services;
+using Sparcpoint.Infrastructure.Services.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Interview.Web
 {
     public class Startup
     {
+        private readonly IConfiguration Configuration;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddMvc()
+               .AddJsonOptions(
+                   options =>
+                   {
+                       options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+                       options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                   }
+               );
+
+            // EVAL: Use this to add the JWT authentication for the API endpoints.
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                        .AddJwtBearer(options =>
+                        {
+                            var issuers = Configuration["Authentication:JWTIssuer"].Split(',');
+                            var validAudiences = Configuration["Authentication:JWTAudience"].Split(',');
+
+                            options.TokenValidationParameters = new TokenValidationParameters
+                            {
+                                ValidateIssuer = true,
+                                ValidateAudience = true,
+                                ValidateLifetime = true,
+                                ValidateIssuerSigningKey = true,
+                                ValidAudiences = validAudiences,
+                                ValidIssuers = issuers,
+                                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Authentication:IssuerSigningKey"])),
+                                ClockSkew = TimeSpan.Zero
+                            };
+
+                            options.Events = new JwtBearerEvents()
+                            {
+                                OnAuthenticationFailed = context =>
+                            {
+                                return context.Response.WriteAsync("Authentication Failed");
+                            },
+                                OnForbidden = context =>
+                            {
+                                return context.Response.WriteAsync("Unauthorized");
+                            }
+                            };
+                        });
+
+            // EVAL: Use this to register the connection to database.
+            services.AddDbContext<SparcpointBaseContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            // EVAL: Use this to register the services.
+            services.AddScoped<IProductService, ProductService>();
+            // services.AddScoped<ICategoryService, CategoryService>();
+            // services.AddScoped<IInventoryTransactionService, InventoryTransactionService>();
+            // services.AddScoped<IInventoryService, InventoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +93,11 @@ namespace Interview.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("./v1/swagger.json", "My API V1");
+                });
             }
             else
             {
@@ -44,8 +110,11 @@ namespace Interview.Web
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseCors();
 
-            app.UseAuthorization();
+            // EVAL: Enable the authentication for the API endpoints.
+            //app.UseAuthentication();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
