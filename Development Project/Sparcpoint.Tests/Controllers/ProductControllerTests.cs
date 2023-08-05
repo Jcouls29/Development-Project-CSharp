50e460b74ecb0b24451ba142d72c33de67cd40e7
csm <csmhowitzer@gmail.com>
2023-08-04 21:14:16 -0400

    Adding in a test project so we can test the expected results for different scenarios

---
+++ Development Project/Sparcpoint.Tests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using static Xunit.Assert;
+namespace Sparcpoint.Tests.Controllers
+{
+	public class ProductControllerTests
+	{
+		// do we want to return a message or an error or just an empty dataset?
+		[Fact(DisplayName = "When No products are in the table. Then return an empty list.")]
+		public void WhenNoItemsAreInTheTable_ThenReturnEmptyList()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			Equal("", "I'm a test, and I'm not configured yet!!");
+		}
+
+		[Fact(DisplayName = "When products are are in the table then return a list of products. ")]
+		public void WhenProductsAreInTheTable_ThenReturnAListOfProducts()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			Equal("", "I'm a test, and I'm not configured yet!!");
+		}
+
+		[Fact(DisplayName = "When a product has a category. Return uri for getting the category data.")]
+		public void WhenProductHasCategory_ThenProductCategoryWillShowAUriForTheCategory()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+			Equal("", "I'm a test, and I'm not configured yet!!");
+		}
+	}
+}
---
+++ Development Project/Sparcpoint.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sparcpoint.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sparcpoint.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e546c13-1d2c-4f55-8d57-7039b3241976")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
