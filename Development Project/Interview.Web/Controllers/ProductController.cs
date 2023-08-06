0330315a2fbc8591f7264a0f49ede64984434d6e
sajyo6 <learnjyo6@gmail.com>
2023-08-06 13:01:23 -0400

    adding controller logic for add

--- Development Project/Interview.Web/Controllers/ProductController.cs
+++ Development Project/Interview.Web/Controllers/ProductController.cs
@@ -1,18 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Web.Http;
 using System.Threading.Tasks;
+using Inventory.BusinessServices;
+using Inventory.BusinessServices.Services;
 
 namespace Interview.Web.Controllers
 {
-    [Route("api/v1/products")]
-    public class ProductController : Controller
+    public class ProductController : ApiController
     {
-        [HttpGet]
-        public Task<IActionResult> GetAllProducts()
+        private IProductService _productService;
+        public ProductController(IProductService productService)
         {
-            return Task.FromResult((IActionResult)Ok(new object[] { }));
+            _productService = productService;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IHttpActionResult> AddProductAsync([System.Web.Http.FromBody] Product product)
+        {
+            var response = await _productService.AddProductAsync(product);
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok(response);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [System.Web.Http.HttpPost]
+        public async Task<IHttpActionResult> AddTransactionAsync( InventoryTransaction inventoryTransaction)
+        {
+            var response = await _productService.AddTransactionAsync(inventoryTransaction);
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok(response);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteTransactionAsync(int transactionId)
+        {
+            var response =  _productService.DeleteTransactionAsync(transactionId);
+            return Ok(response);
         }
     }
 }
