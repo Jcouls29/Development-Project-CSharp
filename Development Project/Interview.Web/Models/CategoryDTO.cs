b47cc24b6babca2d2294bc614702535299b324ae
nik <komakozyozzy@gmail.com>
2023-08-23 06:40:52 -0500

    add models

---
+++ Development Project/Interview.Web/Models/CategoryDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Interview.Web.Models
+{
+    [Table("CategoryAttributes")]
+    public class CategoryDTO
+    {
+        [Key]
+        public int InstanceId { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public DateTime CreatedDateStamp { get; set; }
+    }
+}
