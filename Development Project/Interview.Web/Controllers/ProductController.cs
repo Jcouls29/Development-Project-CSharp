3476b0d1247803b72377e71042927f3ee31fa6c3
justinprogdev <106097398+justinprogdev@users.noreply.github.com>
2024-01-17 20:57:03 -0500

    initial commit: Made placeholder endpoints to the product controller. Made Product Management Services as an intermediary between controller and sql, and registered it at startup.

--- Development Project/Interview.Web/Controllers/ProductController.cs
+++ Development Project/Interview.Web/Controllers/ProductController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Sparcpoint.Core.Models;
 using System.Threading.Tasks;
 
 namespace Interview.Web.Controllers
@@ -9,11 +7,47 @@ namespace Interview.Web.Controllers
     [Route("api/v1/products")]
     public class ProductController : Controller
     {
-        // NOTE: Sample Action
-        [HttpGet]
-        public Task<IActionResult> GetAllProducts()
+        private readonly IProductManagementService _productManagementService;
+
+        /// <summary>
+        /// The constructor for the ProductController is used to inject the IProductManagementService
+        /// </summary>
+        /// <param name="productManagementService"></param>
+        public ProductController(IProductManagementService productManagementService)
         {
-            return Task.FromResult((IActionResult)Ok(new object[] { }));
+            _productManagementService = productManagementService;
+        }
+
+        /// <summary>
+        /// This method is used to return all products
+        /// </summary>
+        /// <remarks>All crud operations are handled by the productManagementService</remarks>
+         public async Task<IActionResult> Products()
+        {
+            var products = await _productManagementService.GetAllProductsAsync();
+            return View(products);
+        }
+
+        /// <summary>
+        /// Get a single product and its attributes by name
+        /// </summary>
+        /// <param name="name"></param>
+        [HttpGet("{name}")]
+        public async Task<Product> GetProduct(string name)
+        {
+            return await _productManagementService.GetProductAsync(name);
+        }
+
+        /// <summary>
+        /// Add a product to the database
+        /// </summary>
+        /// <param name="product"></param>
+        [HttpPost]
+        public async Task<IActionResult> AddProductAsync([FromBody] Product product)
+        {
+            // EVAL: Add product and return 201 Created
+            await _productManagementService.AddProductAsync(product);
+            return CreatedAtAction(nameof(AddProductAsync), new { product.Name }, product);
         }
     }
 }
