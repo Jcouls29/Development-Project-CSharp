5a4231e5340ab5207e08c3fec47329f25fc66fdb
Janatbek Orozaly <janatbek@gmail.com>
2024-03-18 19:37:07 -0600

    creates new project for Data access

---
+++ Development Project/Sparkpoint.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sparkpoint.Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
