afc4ed02f9c9dc651a340a6e9d3baec62578464a
Ezequiel Gonzalez <evazquez@clearcompany.com>
2026-04-16 09:58:07 -0300

    dtos and service structure

---
+++ Development Project/Sparcpoint.Core/Services/InventoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sparcpoint.Services
+{
+    public interface IInventoryService
+    {
+
+    }
+
+    public class InventoryService
+    {
+    }
+}
---
+++ Development Project/Sparcpoint.Core/Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Sparcpoint.Models.DTOs;
+
+namespace Sparcpoint.Services
+{
+    public interface IProductService
+    {
+        Task CreateProduct(ProductDto product);
+    }
+    public class ProductService : IProductService
+    {
+        public ProductService() { }
+
+        public Task CreateProduct(ProductDto product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
