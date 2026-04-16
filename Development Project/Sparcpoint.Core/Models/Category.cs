bac621677ecce3a2909090ec5d32a1589acdb74f
Ezequiel Gonzalez <evazquez@clearcompany.com>
2026-04-16 09:50:30 -0300

    models

---
+++ Development Project/Sparcpoint.Core/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sparcpoint.Models
+{
+    public class Category
+    {
+        public int InstanceId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreatedTimestamp { get; private set; }
+    }
+}
---
+++ Development Project/Sparcpoint.Core/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sparcpoint.Models
+{
+    public class Product
+    {
+        public int InstanceId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public List<string> ProductImageUris { get; private set; } = new List<string>();
+        public List<string> ValidSkus { get; private set; } = new List<string>();
+        public List<Category> Categories { get; private set; } = new List<Category>();
+        public Dictionary<string, string> Metadata { get; private set; } = new Dictionary<string, string>();
+    }
+}
