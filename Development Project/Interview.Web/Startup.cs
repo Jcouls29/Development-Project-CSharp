1ca689c2c2c480ec50a4a594b5b17a01cdb677de
julio.montenegro <julio.montenegro@globallogic.com>
2026-04-20 15:46:35 -0500

    Removed EFCore

--- Development Project/Interview.Web/Startup.cs
+++ Development Project/Interview.Web/Startup.cs
@@ -2,7 +2,6 @@ using Interview.Web.Contracts;
 using Interview.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
